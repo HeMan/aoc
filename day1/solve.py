e0ddc468ad9a1fe7c1d36b2dc28050239840e60d
Jimmy Hedman <jimmy.hedman@gmail.com>
2020-12-05 01:41:12 +0100

    Rename and add timing.

--- day1/solve.py
+++ day1/solve.py
@@ -1,6 +1,4 @@
-with open("input") as f:
-    data = sorted([int(line.rstrip()) for line in f])
-
+import utime
 
 def part1(data):
     head, *tail = data
@@ -22,6 +20,19 @@ def part2(data):
             head2, *tail2 = tail2
         head1, *tail1 = tail1
 
+t0 = utime.ticks_us()
 
+with open("day1/input") as f:
+    data = sorted([int(line.rstrip()) for line in f])
+
+t1 = utime.ticks_us()
 print(part1(data))
+t2 = utime.ticks_us()
 print(part2(data))
+t3 = utime.ticks_us()
+
+print(utime.ticks_diff(t1,t0))
+print(utime.ticks_diff(t2,t1))
+print(utime.ticks_diff(t3,t2))
+
+print(utime.ticks_diff(t3,t0))
